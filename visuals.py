2beefc9a23ac9c53e38fc8d8f73aa9b712afe703
ocasium3 <monicamarquez.p97@gmail.com>
2022-12-02 16:58:34 +0100

    update visuals + functionalities

--- visuals.py
+++ visuals.py
@@ -59,42 +59,46 @@ class VisualsScreen:
         style.map('TButton', foreground = [('active', '!disabled', 'green')], background = [('active', 'black')])
 
         # This is the second button which is used to Quit the GUI
-        quit_button = Button(root, text="Quit",  command=root.destroy, bg= "black",fg = "white", font=('ariel', 11, 'bold'),)       
+        quit_button = Button(root, text="Quit",  borderwidth = 1, relief = "solid",command=root.destroy, bg= "black",fg = "white", font=('ariel', 11, 'bold'),)       
         # placing the Quit button on the screen
         quit_button.place(x=850,y=50)
 
     def labels(self):
 
-        description = tk.Label(canvas1, text='The Eco-Bot is a ML&CS FG Python Script that help us clean our folders from files we do not use anymore', bg = "white", font = ('ariel', 10,'bold'))
+        description = tk.Label(canvas1, text='The Eco-Bot is a ML&CS FG Python Script that help us clean our folders from files we do not use anymore', 
+        borderwidth = 1, relief = "solid",bg = "white", font = ('ariel', 10,'bold'))
         description.place(x = 10, y = 50)
 
         """ LABEL FOR FILE SIZE """
         #Label for the question
-        label2 = tk.Label(canvas1, text='What do you consider as too many bytes?', bg = "white")
+        label2 = tk.Label(canvas1, text='What do you consider as too many bytes?',
+        width = 40, borderwidth = 1, relief = "solid",bg = "white")
 
         label2.config(font=('ariel', 10))
         label2.place(x= 10,y=90) #canvas1.create_window(400, 75, window=label2)
 
         #Entry for the number of bytes
-        entry1 = tk.Entry (canvas1)
+        entry1 = tk.Entry (canvas1, borderwidth = 1, relief = "solid")
         #Defining default value
         entry1.insert(0, "2000000")
-        entry1.place(x = 10, y = 120)#canvas1.create_window(400, 100, window=entry1)
+        entry1.place(x = 100, y = 120)#canvas1.create_window(400, 100, window=entry1)
 
         """ LABEL FOR KEYWORD """
         #Label for the question
-        label2 = tk.Label(canvas1, text='Keyword of files you want to resize/delete', bg = "white")
+        label2 = tk.Label(canvas1, text='Keyword of files you want to resize/delete', 
+        width = 40, borderwidth = 1, relief = "solid", bg = "white")
         label2.config(font=('ariel', 10))
         label2.place(x= 10, y = 160) #canvas1.create_window(400, 125, window=label2)
 
         #Entry for the number of bytes
-        entry2 = tk.Entry (canvas1)
+        entry2 = tk.Entry (canvas1, borderwidth = 1, relief = "solid")
         entry2.insert(0, "") #Defining default value
-        entry2.place(x = 10, y =190)#canvas1.create_window(400, 150, window=entry2)
+        entry2.place(x = 100, y =190)#canvas1.create_window(400, 150, window=entry2)
 
         """ LABEL FOR DATE PICKER"""
         #Label for the question
-        label2 = tk.Label(canvas1, text='Select the date you want to check files previous from', bg = "white")
+        label2 = tk.Label(canvas1, text='Select the date you want to check files previous from',
+        width = 40,borderwidth = 1, relief = "solid", bg = "white")
         label2.config(font=('ariel', 10))
         label2.place(x= 10, y = 220) #canvas1.create_window(400, 125, window=label2)
 
@@ -105,9 +109,8 @@ class VisualsScreen:
                 background='dark green',
                 foreground='dark blue',
                 arrowcolor='white')
-        dateentry = DateEntry(canvas1,style='my.DateEntry')
-        dateentry.grid(row=0,column=0,padx=10, pady = 250)
-        #dateentry.pack()
+        dateentry = DateEntry(canvas1, width = 17, style='my.DateEntry')
+        dateentry.grid(row=0,column=0,padx=100, pady = 250)
 
 #Initializing tkimport
 root = tk.Tk()
@@ -118,7 +121,7 @@ root.title("Eco-Bot")
 
 bg = PhotoImage(file = r"C:\Users\monica.marquez\OneDrive - Accenture\Documents\GitHub\eco-bot\bg.png") 
 #Creating the canvas for the complete dashboard  
-canvas1 = tk.Canvas(root, width = 900, height = 565)
+canvas1 = tk.Canvas(root, width = 900, height = 500)
 #Display Image
 canvas1.create_image(0,0, image = bg, anchor = "nw")
 canvas1.pack(fill = "both", expand = True)
